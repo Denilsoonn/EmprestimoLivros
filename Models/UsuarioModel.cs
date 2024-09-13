68034a448a0b2b7f2f8749a4234b6bc3451ed48e
Denilson Barbosa <108957598+Denilsoonn@users.noreply.github.com>
2024-09-13 12:16:50 -0300

    Models

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class UsuarioModel
+    {
+
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Sobrenome { get; set; }
+        public required string Email { get; set; }
+        public required byte[] SenhaHash { get; set; }
+        public required byte[] SenhaSalt { get; set; }
+
+    }
+}
