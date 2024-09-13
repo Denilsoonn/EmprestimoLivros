fd01439646cf1128c715191af3c0fa04a0fd4a3c
Denilson Barbosa <108957598+Denilsoonn@users.noreply.github.com>
2024-09-13 12:17:21 -0300

    Dto

---
+++ Dto/UsuarioLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Dto
+{
+    public class UsuarioLoginDto
+    {
+        [Required(ErrorMessage = "Digite o Email!")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "Digite a senha!")]
+        public required string Senha { get; set; }
+    }
+}
---
+++ Dto/UsuarioRegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Dto
+{
+    public class UsuarioRegisterDto
+    {
+        [Required(ErrorMessage = "Digite o nome!")]
+        public required string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o sobrenome!")]
+        public required string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Digite o email!")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite a senha!")]
+        public required string Senha { get; set; }
+
+        [Required(ErrorMessage = "Digite a confirmação da senha!"),
+            Compare("Senha", ErrorMessage = "As senhas não estão iguais")]
+        public required string ConfirmaSenha { get; set; }
+
+    }
+}
